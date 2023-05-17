6772b70d2afc20d287993a2cc8b02a72af5f6e7e
ehdrn3020 <ehdrnsla3020@naver.com>
2023-05-17 10:02:17 +0900

    20230517 add branch function

--- dags/func_modules/branch.py
+++ dags/func_modules/branch.py
@@ -1,30 +1,60 @@
-import pendulum
+import logging
+import requests
+from datetime import datetime
 from airflow.decorators import dag, task
 
+def default_args():
+    default_args = {
+        'owner':'my',
+        'depends_on_past':False,
+        'start_date':datetime(2023, 5, 15),
+    }
+    return default_args
+
 @dag(
-    schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    default_args=default_args(),
+    schedule_interval='30 9 * * *',
     catchup=False,
     tags=["function"]
 )
 
-def branch_func():
+def check_request_exection():
+    @task()
+    def set_date(**kwargs):
+        work_date = kwargs["logical_date"].in_timezone('Asia/Seoul').strftime('%Y%m%d')
+        logging.info(f'work_date:{work_date}')
+        return work_date
+
+    @task()
+    def get_status(work_date:str):
+        url=f'https://your_request_url?region=seoul&date={work_date}'
+        headers = {
+            'Content-Type':'application/json',
+            'API-Key':'123456abcdefg'
+        }
+        result = requests.get(url, headers=headers)
+        response = result.json()
+        logging.info(f'resonse:{response}')
+        result = response.get('result', None)
+        return result
+
     @task.branch()
-    def condition():
-        result = True
-        if result:
-            return 'success_task_func'
+    def condition(status):
+        if status == 'SUCCESS':
+            return 'success_status_task'
         else:
-            return 'failed_task_func'
+            return 'failed_status_task'
 
     @task()
-    def success_task_func():
-        print('SUCCESS')
+    def success_status_task():
+        logging.info('SUCCESS')
 
     @task()
-    def failed_task_func():
-        print('FAILED')
+    def failed_status_task():
+        logging.info('FAILED')
 
-    condition() >> [success_task_func(), failed_task_func()]
+    set_date_task = set_date()
+    get_status_task = get_status(set_date_task)
+    condition(get_status_task) >> [success_status_task(), failed_status_task()]
 
-branch_func()
+check_request_exection()
